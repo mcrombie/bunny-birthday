25efd1b6c896bee8806250958a317ca1fe11db81
Michael Crombie <mcrombie1994@gmail.com>
2019-12-11 20:29:13 -0800

    Sending live to my blog. Hopefully these media queries make it descent on mobile.

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ function animateWave(){
     document.getElementById("arm-left").className = "r-body-part animate-wave";
     
 }
+function animateRaiseLeftArm(){
+    document.getElementById("arm-left").className = "r-body-part animate-raise-left-arm";
+    
+}
+function animateRaiseRightArm(){
+    document.getElementById("arm-right").className = "r-body-part animate-raise-right-arm";
+    
+}
 function animateSpeak(){
     console.log("Make speaking noise")
     document.getElementById("talk-bubble").className = "animate-speak";
@@ -22,7 +30,13 @@ function animateTwitterPate(){
     //     element.classList = "r-body-part animate-jump";
     // })
     console.log("twitterpate")
-    document.getElementById("eye-left-heart").className = "animate-twitterpate";
+    document.getElementById("eye-left-heart").className = "r-body-part animate-twitterpate-left";
+    document.getElementById("eye-right-heart").className = "r-body-part animate-twitterpate-right";
+}
+
+function animateEyeDisappear(){
+    document.getElementById("eye-left").className = "r-body-part animate-eye-disappear";
+    document.getElementById("eye-right").className = "r-body-part animate-eye-disappear";
 }
 
 function resetAnimations(){
@@ -30,6 +44,8 @@ function resetAnimations(){
     document.getElementById("talk-bubble").classList.remove("animate-speak");
     document.getElementById("r-body").classList.remove("animate-jump");
     document.getElementById("eye-left-heart").classList.remove("animate-twitterpate");
+    document.getElementById("eye-left").classList.remove("animate-eye-disappear");
+    document.getElementById("eye-right").classList.remove("animate-eye-disappear");
 
     Array.prototype.forEach.call(document.getElementsByClassName("r-body-part"), function(element){
         element.classList = "r-body-part";
